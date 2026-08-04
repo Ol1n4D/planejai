01252685f5c67b139917196a4e950730e619bd5a
Danilo Silva <db_asilva@hotmail.com>
2026-08-04 11:17:03 -0300

    feat: adicionar configuração do Prettier, ESLint e ajustes no projeto

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettier from 'eslint-plugin-prettier'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import unusedImports from 'eslint-plugin-unused-imports'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      prettier,
+      'unused-imports': unusedImports,
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+  {
+    name: 'prettier-config',
+    rules: {
+      ...eslintConfigPrettier.rules,
+    },
+  },
+])
