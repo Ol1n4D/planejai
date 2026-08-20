25fe7ec3438caa5b004eda84f53c39b6904007fd
Danilo Silva <db_asilva@hotmail.com>
2026-08-19 21:42:35 -0300

    fix: add @src alias to Vite resolve config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 /* eslint-disable prettier/prettier */
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   base: '/planejai/',
+  resolve: {
+    alias: {
+      '@src': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
